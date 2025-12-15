d0a68e6b0a031f6251825084ab97aefcd29c0c66
Marwa <you@example.com>
2025-12-15 16:18:39 -0500

    version finale

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const API_BASE_URL =
-  'refugedechat2-cxd9cgfebzgch0gt.canadacentral-01.azurewebsites.net/api';
+  'https://refugedechat2-cxd9cgfebzgch0gt.canadacentral-01.azurewebsites.net/api';
